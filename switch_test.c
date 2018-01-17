b2bb33aa1c4b190933d76cde556173a0e3c34d8a
Lebranc Convas <lebrancconvas@gmail.com>
2018-01-17 07:22:05 +0700

    C-Library Commit

---
+++ switch_test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int i = 2;
+    switch(i)
+    {
+        case 1:
+            printf("%d",i);
+            break;
+        case 2:
+            printf("%d",i);
+            break;
+        default:
+            printf("%d",i);
+            break;
+    }
+}
